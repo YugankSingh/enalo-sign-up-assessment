486db43de6644b43a32732033cc3f292e915dc18
Yugank Singh <yuganksingh05@gmail.com>
2024-06-17 15:17:35 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss"
+import { Button, Form, Input, Checkbox, ConfigProvider } from "antd"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const onFinish = () => {
+		console.log("On Finish")
+	}
+	const onFinishFailed = () => {
+		console.log("On Finish Failed")
+	}
+
+	return (
+		<div className="App">
+			<ConfigProvider
+				theme={{
+					colorPrimary: "#00b96b",
+				}}
+			>
+				<div className="login">
+					<div className="login-banner"></div>
+					<div className="login-main">
+						<div className="login-page-logo">
+							<img src="/images/logo.png" alt="Logo" />
+						</div>
+						<div className="login-main-inner">
+							<Form
+								name="basic"
+								// labelCol={{ span: 8 }}
+								wrapperCol={{ span: 24 }}
+								style={{ maxWidth: 800 }}
+								initialValues={{ remember: true }}
+								onFinish={onFinish}
+								onFinishFailed={onFinishFailed}
+								autoComplete="off"
+							>
+								<div className="login-form-top">
+									<h1>Sign Up</h1>
+									<p>
+										Or <a href="/sign-in">Sign In</a>
+									</p>
+								</div>
+								<Form.Item
+									name="email"
+									rules={[
+										{ required: true, message: "Please input your email!" },
+									]}
+									className="custom-login-form"
+								>
+									<Input
+										placeholder="Email"
+										size="large"
+										className="custom-input"
+									/>
+								</Form.Item>
+								<Form.Item
+									name="password"
+									rules={[
+										{ required: true, message: "Please input your password!" },
+									]}
+								>
+									<Input
+										placeholder="Password"
+										size="large"
+										className="custom-input"
+									/>
+								</Form.Item>
+								<Form.Item
+									name="password_confirm"
+									rules={[
+										{ required: true, message: "Please input your email!" },
+									]}
+								>
+									<Input
+										placeholder="Re-Enter Password"
+										size="large"
+										className="custom-input"
+									/>
+								</Form.Item>
+
+								<Form.Item name="remember_me">
+									<Checkbox>Remember Me</Checkbox>
+								</Form.Item>
+
+								<Form.Item>
+									<Button
+										type="primary"
+										htmlType="submit"
+										block
+										size="large"
+										className="custom-button"
+										style={{
+											padding: "14px",
+											paddingLeft: "25px",
+											borderRadius: "30px",
+											fontWeight: "bold",
+											height: "calc( 1em + 28px)",
+											backgroundColor: "#17b582",
+										}}
+									>
+										Log In
+									</Button>
+								</Form.Item>
+							</Form>
+						</div>
+					</div>
+				</div>
+			</ConfigProvider>
+		</div>
+	)
 }
 
-export default App;
+export default App
